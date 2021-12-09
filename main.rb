2f5d6d7a410dbef6fa94234d2cdef0e20f0a67db
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-09 14:01:54 +0200

    Create a method to decode the entire message in Morse code

--- main.rb
+++ main.rb
@@ -30,6 +30,7 @@ $alphabet = {
 
 def decode_char(char)
   letter_morse = $alphabet[char].upcase
+
   return letter_morse
 end
 
@@ -37,12 +38,29 @@ puts decode_char(".-")
 
 def decode_word(word)
   new_word = []
+
   split_word = word.split(' ')
+
   split_word.each do |letter|
     new_word.push(decode_char(letter))
   end
+
   return new_word.join
 end
 
 puts decode_word("-- -.--")
+
+def decode(msg)
+  new_msg = []
+
+  split_msg = msg.split('   ')
+
+  split_msg.each do |word|
+    new_msg.push(decode_word(word))
+  end
+
+  return new_msg.join(' ')
+end
+
+puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...")
 
